c28c95e710a803d3b3e77c7b718aa881153e5352
chaz09 <charlynoyakhilome@gmail.com>
2019-04-16 22:30:20 +0200

    finished chat

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,5 +1,5 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
 
   def index
     @users = User.all
@@ -15,8 +15,8 @@ class ConversationsController < ApplicationController
     redirect_to conversation_messages_path(@conversation)
   end
 
-  private
+    private
   def conversation_params
-    params.permit(:sender_id, recipient_id)
+    params.permit(:sender_id, :recipient_id)
   end
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ActiveRecord::Base
-  belongs_to :sender, :foreign_key => :sender_id, class_namme: 'User'
-    belongs_to :recipient, :foreign_key => :recipient_id, class_namme: 'User'
+  belongs_to :sender, :foreign_key => :sender_id, class_name: 'User'
+    belongs_to :recipient, :foreign_key => :recipient_id, class_name: 'User'
 
     has_many :messages, dependent: :destroy
     validates_uniqueness_of :sender_id , :scope => :recipient_id
@@ -11,4 +11,4 @@ class Conversation < ActiveRecord::Base
  	 	  sender_id,recipient_id, recipient_id, sender_id)
 
     end
-  end 
+  end
